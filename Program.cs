d7c7f34c3eab5e67cf2d56f8adea7f276db0b10b
Jaron <jjkenyon777@gmail.com>
2022-04-13 08:57:51 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+   List<string> employees = new List<string>();
+    // Collect user values until the value is an empty string
+   while (true)
+   {
+   Console.WriteLine("Please enter a name: (leave empty to exit)");
+   // Get a name from the console and assign it to a variable
+   string input = Console.ReadLine();
+   if (input == "")
+   {
+     break;
+   }
+   employees.Add(input);
+   }
+   for (int i = 0; i < employees.Count; i++)
+   {
+     Console.WriteLine(employees[i]);
+   }
+    }
+    
+    static List<string> GetEmployees()
+    {
+      // I will return a List of strings
+    }
+  }
+}
