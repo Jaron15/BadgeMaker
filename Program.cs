880bac0fda66bde8b3f732ebef98f51fe16d49bc
Jaron <jjkenyon777@gmail.com>
2022-04-18 14:52:26 -0700

    updated output logic

--- Program.cs
+++ Program.cs
@@ -7,13 +7,13 @@ namespace CatWorx.BadgeMaker
   {
     static void Main(string[] args)
     {
-      List<string> employees = GetEmployees();
+      List<Employee> employees = GetEmployees();
       PrintEmployees(employees);
     }
     
-    static List<string> GetEmployees()
+    static List<Employee> GetEmployees()
     {
-    List<string> employees = new List<string>();
+    List<Employee> employees = new List<Employee>();
     // Collect user values until the value is an empty string
     while (true)
       {
@@ -26,16 +26,17 @@ namespace CatWorx.BadgeMaker
       }
       // Create a new Employee instance
       Employee currentEmployee = new Employee(input, "Smith");
-      employees.Add(currentEmployee.GetName());
+      // Add currentEmployee, not a string
+      employees.Add(currentEmployee);
       }
    return employees;
     }
 
-    static void PrintEmployees(List<string> employees)
+    static void PrintEmployees(List<Employee> employees)
     {
       for (int i = 0; i < employees.Count; i++)
       {
-        Console.WriteLine(employees[i]);
+        Console.WriteLine(employees[i].GetName());
       }
     }
   }
