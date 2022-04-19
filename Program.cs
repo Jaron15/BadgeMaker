157c8ee04bad3f719f4b7dfd310321f904b76299
Jaron <jjkenyon777@gmail.com>
2022-04-19 09:51:58 -0700

    added util class

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace CatWorx.BadgeMaker
     static void Main(string[] args)
     {
       List<Employee> employees = GetEmployees();
-      PrintEmployees(employees);
+      Util.PrintEmployees(employees);
     }
     
     static List<Employee> GetEmployees()
@@ -40,13 +40,5 @@ namespace CatWorx.BadgeMaker
    return employees;
     }
 
-    static void PrintEmployees(List<Employee> employees)
-    {
-      for (int i = 0; i < employees.Count; i++) 
-      {
-        string template = "{0,-10}\t{1,-20}\t{2}";
-        Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
-      }
-    }
   }
 }
