e44d29fc1670968bf8053efec67daf87b7d83ae8
Jaron <jjkenyon777@gmail.com>
2022-04-20 15:01:42 -0700

    final touches

--- PeopleFetcher.cs
+++ PeopleFetcher.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Net;
 using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
 
 namespace CatWorx.BadgeMaker {
     class PeopleFetcher 
     {
-        public static List<Employee> GetEmployees()
+    public static List<Employee> GetEmployees()
     {
     List<Employee> employees = new List<Employee>();
     // Collect user values until the value is an empty string
@@ -29,18 +30,31 @@ namespace CatWorx.BadgeMaker {
       Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
       // Add currentEmployee, not a string
       employees.Add(currentEmployee);
-      }
-
-      using (WebClient client = new WebClient())
-      {
-          // Image example 
-          string response = client.DownloadString("https://randomuser.me/api/?results=10&nat=us&inc=name,id,picture");
-          Console.WriteLine(response);
-
-      }
+      }   
    return employees;
     }
 
-    // static void GetFromAPI
+    public static List<Employee> GetFromAPI()
+      {
+        List<Employee> employees = new List<Employee>();
+        using (WebClient client = new WebClient())
+      {
+          // Image example 
+          string response = client.DownloadString("https://randomuser.me/api/?results=10&nat=us&inc=name,id,picture");
+          JObject json = JObject.Parse(response);
+          foreach (JToken token in json.SelectToken("results")) {
+              // Parse JSON data
+              Employee emp = new Employee
+              (
+                  token.SelectToken("name.first").ToString(), 
+                  token.SelectToken("name.last").ToString(),
+                  Int32.Parse(token.SelectToken("id.value").ToString().Replace("-", "")),
+                  token.SelectToken("picture.large").ToString()
+              );
+              employees.Add(emp);
+          }
+      }
+      return employees;
+      }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,10 +10,21 @@ namespace CatWorx.BadgeMaker
     {
       List<Employee> employees = new List<Employee>();
 
-      employees = PeopleFetcher.GetEmployees();
-
+      while (true) {
+      Console.WriteLine("Would you like to create badges with the data from the API? If so enter 'yes'. To create a custom badge enter 'no'");
+      string response = Console.ReadLine();
+      if (response == "yes") {
+      employees = PeopleFetcher.GetFromAPI();
+      }
+      else if (response == "no") {
+        employees = PeopleFetcher.GetEmployees();
+      }
+      else {
+        Console.WriteLine("Please enter a valid response");
+      }
       Util.MakeCSV(employees);
       Util.MakeBadges(employees);
+      }
     }
 
   }
